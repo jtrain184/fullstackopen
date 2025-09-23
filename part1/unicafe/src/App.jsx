505616209efe1cbb546c9f029a323c7287758880
jtrain184 <pejarrett@gmail.com>
2025-09-23 17:43:23 -0400

    1.6 Add buttons to collect feedback

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,10 +6,29 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGood = () =>
+    setGood( good + 1)
+
+  const handleNeutral = () =>
+    setNeutral(neutral + 1)
+
+  
+  const handleBad = () =>
+    setBad( bad + 1 )
+
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <button onClick={handleGood} text='good'>good</button> 
+      <button onClick={handleNeutral} text='neutral'>netural</button>
+      <button onClick={handleBad} text='bad'>bad</button>
+      <h1>statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
     </div>
+
+
   )
 }
 
