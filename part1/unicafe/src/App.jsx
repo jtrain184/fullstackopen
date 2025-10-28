88950fd897c75b9a139b7cfc298143616caaf4db
jtrain184 <pejarrett@gmail.com>
2025-10-28 18:07:04 -0400

    1.8 step 3 - Add statistics component

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,19 @@
 import { useState } from 'react'
 
+const Statistics = (props) => {
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>Good: {props.good}</p>
+      <p>Neutral: {props.neutral}</p>
+      <p>Bad: {props.bad}</p>
+      <p>All: {props.total}</p>
+      <p>Average: {(props.good * 1 + props.bad * -1) / props.total}</p>
+      <p>Positive: {props.good / props.total}%</p>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -7,29 +21,23 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
   const handleGood = () =>
-    setGood( good + 1)
+    setGood(good + 1)
 
   const handleNeutral = () =>
     setNeutral(neutral + 1)
 
   const handleBad = () =>
-    setBad( bad + 1 )
+    setBad(bad + 1)
 
   const total = good + bad + neutral
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={handleGood} text='good'>good</button> 
+      <button onClick={handleGood} text='good'>good</button>
       <button onClick={handleNeutral} text='neutral'>netural</button>
       <button onClick={handleBad} text='bad'>bad</button>
-      <h1>statistics</h1>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>All: {total}</p>
-      <p>Average: {(good*1 + bad*-1)/total}</p>
-      <p>Positive: {good/total}%</p>
+      <Statistics good={good} total={total} bad={bad} neutral={neutral}/>
     </div>
 
 
